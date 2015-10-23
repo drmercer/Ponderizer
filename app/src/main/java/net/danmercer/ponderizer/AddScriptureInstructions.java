cb00249cf2d6a6e720719083d6c06a20f9986eb0
Dan Mercer <danielmercer1@gmail.com>
2015-10-23 16:26:54 -0600

    Polishing the AddScriptureInstructions
    Minor improvements.

--- app/src/main/java/net/danmercer/ponderizer/AddScriptureInstructions.java
+++ app/src/main/java/net/danmercer/ponderizer/AddScriptureInstructions.java
@@ -20,16 +20,13 @@ public class AddScriptureInstructions extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_scripture_instructions);
 
-        ImageView demo = (ImageView) findViewById(R.id.how_to_add_view);
-        AnimationDrawable ad = (AnimationDrawable) demo.getDrawable();
-        ad.start();
-
         Button b = (Button) findViewById(R.id.instruction_button);
         if (isGospelLibraryInstalled()) {
             b.setText("Launch Gospel Library app");
             b.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    finish(); // End this activity
                     // Launch Gospel Library app
                     Intent i = getPackageManager().getLaunchIntentForPackage(GOSPEL_LIBRARY_PACKAGE_NAME);
                     startActivity(i);
@@ -45,12 +42,17 @@ public class AddScriptureInstructions extends AppCompatActivity {
                 }
             });
         }
+
+        ImageView demo = (ImageView) findViewById(R.id.how_to_add_view);
+        AnimationDrawable ad = (AnimationDrawable) demo.getDrawable();
+        ad.start();
     }
 
     /**
      * Launches the Play store (or a browser as fallback) to install the Gospel Library app.
      */
     private void installGospelLibrary() {
+        finish();
         try{
             // Try to open using the Play app directly
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(
@@ -66,7 +68,7 @@ public class AddScriptureInstructions extends AppCompatActivity {
     private boolean isGospelLibraryInstalled() {
         PackageManager pm = getPackageManager();
         try{
-            pm.getPackageInfo("org.lds.ldssa", PackageManager.GET_ACTIVITIES);
+            pm.getPackageInfo(GOSPEL_LIBRARY_PACKAGE_NAME, PackageManager.GET_ACTIVITIES);
             return true;
         } catch (PackageManager.NameNotFoundException e) {
             return false;
